ac2b0ef2d37726653847177b8aa4c1f52b3d75b6
tchen <chenming8302@gmail.com>
2021-05-19 16:21:00 +0000

    update ftp script

--- script/start-ftp.sh
+++ script/start-ftp.sh
@@ -5,4 +5,6 @@ docker run -d -v  /home/ubuntu/www/Hilton/laravel/public/:/home/vsftpd \
 -e FTP_USER=bosco -e FTP_PASS=hilton \
 -e PASV_ADDRESS=172.31.13.190 \
 -e PASV_MIN_PORT=21100 -e PASV_MAX_PORT=21110 \
+-e FILE_OPEN_MODE=0666 \
+-e LOCAL_UMASK=0 \
 --name ftp --restart=always fauria/vsftpd
