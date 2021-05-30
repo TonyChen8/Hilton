ad32c4057ba4f44c0acc60bbe9f4673f40d54ba8
chen ming <chenming8302@gmail.com>
2021-05-30 22:57:17 +1000

    build v1.0.0

--- laravel/routes/home.php
+++ laravel/routes/home.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\StaffsController;
 Route::get('/', function () { return view('home'); })->name('home');
 Route::get('/service', function () { return view('service'); })->name('service');
 Route::get('/ladies', function () { return view('ladies'); })->name('ladies');
-Route::get('/whoson', function () { return view('whosOn'); })->name('whosOn');
-Route::get('/contact', function () { return view('contactUs'); })->name('contactUs');
+Route::get('/whoson', function () { return view('whosOn'); })->name('whoson');
+Route::get('/contact', function () { return view('contactUs'); })->name('contactus');
 
 Route::get('/staffs', [StaffsController::class, 'staffs']);
