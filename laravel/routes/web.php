4b25ace65cff90f74f57f19745dbfbb34a584318
chen ming <chenming8302@gmail.com>
2021-07-06 23:21:14 +1000

    clean routes and build

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -39,9 +39,7 @@ Route::group(
     Route::post('/schedule', [ScheduleController::class, 'store']);
     Route::get('/schedules', [ScheduleController::class, 'schedules']);
 
-    Route::get('/notices', [NoticesController::class, 'index'])->name(
-      'notices'
-    );
+    Route::get('/notices', [NoticesController::class, 'index'])->name('notices');
     Route::post('/notice', [NoticesController::class, 'store']);
     Route::delete('/notice/{id}', [NoticesController::class, 'delete']);
     Route::put('/notice/{id}', [NoticesController::class, 'update']);
@@ -53,14 +51,14 @@ Route::group(['domain' => 'www.angels26.com.au'], function () {
   require __DIR__ . '/home.php';
 });
 
-Route::group(['domain' => 'localhost'], function () {
-  require __DIR__ . '/home.php';
-  Route::get('/notices', [NoticesController::class, 'index'])->name('notices');
-  Route::post('/notice', [NoticesController::class, 'store']);
-  Route::delete('/notice/{id}', [NoticesController::class, 'delete']);
-  Route::put('/notice/{id}', [NoticesController::class, 'update']);
-  Route::get('/notice/all', [NoticesController::class, 'notices']);
-});
+// Route::group(['domain' => 'localhost'], function () {
+//   require __DIR__ . '/home.php';
+//   Route::get('/notices', [NoticesController::class, 'index'])->name('notices');
+//   Route::post('/notice', [NoticesController::class, 'store']);
+//   Route::delete('/notice/{id}', [NoticesController::class, 'delete']);
+//   Route::put('/notice/{id}', [NoticesController::class, 'update']);
+//   Route::get('/notice/all', [NoticesController::class, 'notices']);
+// });
 
 // Route::group(['domain' => 'www.angels26.com.au'], function () {
 //  Route::get('/', function () {
