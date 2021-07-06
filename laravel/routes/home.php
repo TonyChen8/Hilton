ed8eefb62707da918cd4ed6359017dcd9b0dc27e
chen ming <chenming8302@gmail.com>
2021-07-06 23:29:07 +1000

    Api route issue

--- laravel/routes/home.php
+++ laravel/routes/home.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaffsController;
+use App\Http\Controllers\NoticesController;
 
 Route::get('/', function () { return view('home'); })->name('home');
 Route::get('/service', function () { return view('service'); })->name('service');
@@ -10,3 +11,4 @@ Route::get('/whoson', function () { return view('whoson'); })->name('whoson');
 Route::get('/contact', function () { return view('contactus'); })->name('contactus');
 
 Route::get('/staffs', [StaffsController::class, 'staffs']);
+Route::get('/notice/all', [NoticesController::class, 'notices']);
