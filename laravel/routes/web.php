8e44f808a3778f2c40d6ab513bad8f44fa847799
chen ming <chenming8302@gmail.com>
2021-09-04 01:51:10 +1000

    close dev routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -52,15 +52,15 @@ Route::group(['domain' => 'www.angels26.com.au'], function () {
 });
 
 
-// 调试开发用的
-Route::group(['domain' => 'localhost'], function () {
-  require __DIR__ . '/home.php';
-  Route::get('/notices', [NoticesController::class, 'index'])->name('notices');
-  Route::post('/notice', [NoticesController::class, 'store']);
-  Route::delete('/notice/{id}', [NoticesController::class, 'delete']);
-  Route::put('/notice/{id}', [NoticesController::class, 'update']);
-  Route::get('/notice/all', [NoticesController::class, 'notices']);
-});
+// // 调试开发用的
+// Route::group(['domain' => 'localhost'], function () {
+//   require __DIR__ . '/home.php';
+//   Route::get('/notices', [NoticesController::class, 'index'])->name('notices');
+//   Route::post('/notice', [NoticesController::class, 'store']);
+//   Route::delete('/notice/{id}', [NoticesController::class, 'delete']);
+//   Route::put('/notice/{id}', [NoticesController::class, 'update']);
+//   Route::get('/notice/all', [NoticesController::class, 'notices']);
+// });
 
 
 // 维护用的页面
